bef625ca7b37786eac045b1a9ea31aa188878c9a
Polina <polinausolceva44@gmai.com>
2024-11-28 15:36:34 +0300

    новая схема бд

--- Server/src/main/resources/db/schema.sql
+++ Server/src/main/resources/db/schema.sql
@@ -1,4 +1,5 @@
 create database cp5;
+use cp5;
 
 create table user
 (
@@ -11,25 +12,24 @@ create table user
 
 create table client
 (
-    id    int          not null primary key
-        references user (id),
+    id    int          not null primary key,
     name  varchar(255) not null,
     email varchar(255) not null,
-    phone varchar(255) not null
+    phone varchar(255) not null,
+    constraint foreign key (id) references user (id)
 );
 
 create table payment_method
 (
     id          int          not null primary key auto_increment,
-    client_id   int          not null
-        references client (id),
+    client_id   int          not null,
     card_number varchar(255) not null
         check (card_number regexp '^[0-9]{16}$'),
     expires_at  date         not null,
     cardholder  varchar(255) not null,
     cvv         varchar(255) not null
-        check (cvv regexp '^[0-9]{3}$')
-
+        check (cvv regexp '^[0-9]{3}$'),
+    constraint foreign key (client_id) references client (id)
 );
 
 create table film
@@ -49,14 +49,14 @@ create table film
 create table review
 (
     id          int      not null primary key auto_increment,
-    film_id     int      not null
-        references film (id),
-    client      int      not null
-        references client (id),
-    rating      int      not null
-        check (rating > 0 and rating < 6),
+    film_id     int      not null,
+    client_id   int      not null,
+    rating      int      not null,
+    check (rating > 0 and rating < 6),
     description varchar(255),
-    created_at  datetime not null default now()
+    created_at  datetime not null default now(),
+    constraint foreign key (film_id) references film (id),
+    constraint foreign key (client_id) references client (id)
 );
 
 create table cinema
@@ -66,42 +66,42 @@ create table cinema
     address varchar(255) not null
 );
 
-create table cinema_hall
+create table hall
 (
     id        int          not null primary key auto_increment,
-    cinema_id int          not null
-        references cinema (id),
+    cinema_id int          not null,
     name      varchar(255) not null,
     seats     int          not null
         check ( seats > 0 and seats % 10 = 0 ),
     price     float        not null
-        check ( price > 0 )
+        check ( price > 0 ),
+    constraint foreign key (cinema_id) references cinema (id)
 );
 
-create table cinema_session
+create table session
 (
     id        int      not null primary key auto_increment,
-    film_id   int      not null
-        references film (id),
-    cinema_id int      not null
-        references cinema (id),
-    hall_id   int      not null
-        references cinema_hall (id),
-    date      datetime not null
+    film_id   int      not null,
+    cinema_id int      not null,
+    hall_id   int      not null,
+    date      datetime not null,
+    constraint foreign key (film_id) references film (id),
+    constraint foreign key (cinema_id) references cinema (id),
+    constraint foreign key (hall_id) references hall (id)
 );
 
 create table `order`
 (
     id                int          not null primary key auto_increment,
-    client_id         int          not null
-        references client (id),
-    payment_method_id int          not null
-        references payment_method (id),
-    cinema_session_id int          not null
-        references cinema_session (id),
+    client_id         int          not null,
+    payment_method_id int          not null,
+    session_id        int          not null,
     seat_index        int          not null
         check (seat_index > 0),
     status            varchar(255) not null default 'payed'
         check (status = 'payed' or status = 'canceled'),
-    created_at        datetime     not null default now()
+    created_at        datetime     not null default now(),
+    constraint foreign key (client_id) references client (id),
+    constraint foreign key (payment_method_id) references payment_method (id),
+    constraint foreign key (session_id) references session (id)
 );
